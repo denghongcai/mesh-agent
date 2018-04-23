407b7e571a4b9fbc25b1a07db047d6a823218b22
hongcai.dhc <hongcai.dhc@alibaba-inc.com>
2018-04-23 23:37:26 +0800

    f

--- consumer/rpc/router.go
+++ consumer/rpc/router.go
@@ -9,15 +9,15 @@ func selectRoute(chances []int64) int {
 	if chancesLength == 1 {
 		return 0
 	}
-	var sumOfChances int64
+	var sumOfChances float64
 	for _, chance := range chances {
-		sumOfChances = sumOfChances + chance
+		sumOfChances = sumOfChances + (1 / float64(chance))
 	}
 
-	randomFloat := rand.Float64() * float64(sumOfChances)
+	randomFloat := rand.Float64() * sumOfChances
 
-	for float64(sumOfChances) > randomFloat {
-		sumOfChances = sumOfChances - chances[chancesLength -1]
+	for sumOfChances > randomFloat {
+		sumOfChances = sumOfChances - (1 / float64(chances[chancesLength -1]))
 		chancesLength = chancesLength - 1
 	}
 
